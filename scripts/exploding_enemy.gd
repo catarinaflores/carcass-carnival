6cc2e8830236d04b663535ab4e0ff93b3620ffd2
Dartk0 <duarte.silvalopes@gmail.com>
2025-02-22 10:34:27 +0000

    amos na mesma sala se querem saber o que eu fiz pergunte-me

---
+++ scripts/exploding_enemy.gd
@@ -0,0 +1,21 @@
+extends Area2D
+@onready var player: Area2D = $"../Player"
+const XplosionDistance = 10
+var speed = 50
+@onready var explode = preload("res://scenes/explosion.tscn").instantiate()
+
+func _process(delta: float) -> void:
+	explode.position.x = position.x
+	explode.position.y = position.y
+	if abs(player.position.x - position.x) + abs(player.position.y - position.y) < XplosionDistance:
+		get_tree().get_root().add_child(explode)
+		queue_free()
+	else:
+		if(player.position.x > position.x):
+			position.x += speed * delta
+		else:
+			position.x -= speed * delta
+		if(player.position.y > position.y):
+			position.y += speed * delta
+		else:
+			position.y -= speed * delta
